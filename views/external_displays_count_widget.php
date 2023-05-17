70fa92bfe46105f33733335791587deaeb765b94
tuxudo <tuxudo@users.noreply.github.com>
2023-05-17 11:53:40 -0400

    v6 style

--- views/external_displays_count_widget.php
+++ views/external_displays_count_widget.php
@@ -1,30 +1,22 @@
 <div class="col-lg-4 col-md-6">
-
-	<div class="panel panel-default" id="external-displays-count-widget">
-
-		<div class="panel-heading" data-container="body">
-
-			<h3 class="panel-title"><i class="fa fa-expand"></i>
-			    <span data-i18n="displays_info.widget_title"></span>
-			    <list-link data-url="/show/listing/displays_info/displays"></list-link>
-			</h3>
-
+	<div class="card" id="external-displays-count-widget">
+		<div class="card-header" data-container="body">
+			<i class="fa fa-expand"></i>
+			<span data-i18n="displays_info.widget_title"></span>
+			<a href="/show/listing/displays_info/displays" class="pull-right"><i class="fa fa-list"></i></a>
 		</div>
-
-		<div class="panel-body text-center"></div>
-
-	</div><!-- /panel -->
-
+		<div class="card-body text-center"></div>
+	</div>
 </div><!-- /col -->
 
 <script>
 $(document).on('appReady', function(e, lang) {
-	$('#external-displays-count-widget div.panel-heading')
+	$('#external-displays-count-widget div.card-header')
 		.attr('title', i18n.t('displays_info.widget_info'))
 		.tooltip();
 });
 $(document).on('appUpdate', function(e, lang) {
-	var body = $('#external-displays-count-widget div.panel-body');
+	var body = $('#external-displays-count-widget div.card-body');
 
 	$.getJSON( appUrl + '/module/displays_info/get_count/1', function( data ) {
 
@@ -33,6 +25,5 @@ $(document).on('appUpdate', function(e, lang) {
 		data.total = data.total || 0;
 		body.append('<a href="'+appUrl+'/show/listing/displays_info/displays#external" class="btn btn-success"><span class="bigger-150">'+data.total+'</span><br>'+i18n.t('displays_info.displays')+'</a>');
 	});
-
 });
 </script>
